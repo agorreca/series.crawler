239802ef1720f76e4a3021bc9c927a26945aa3e6
Nicolas Agorreca <agorreca@gmail.com>
2012-11-13 01:13:57 -0200

    Adding connection through TOR network

--- grails-app/domain/seriescrawler/TvLinks.groovy
+++ grails-app/domain/seriescrawler/TvLinks.groovy
@@ -4,17 +4,17 @@ import java.text.SimpleDateFormat
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 
-import org.apache.commons.lang.StringUtils;
-import org.series.crawler.HTTP
+import org.apache.commons.lang.StringUtils
 
 class TvLinks extends Site {
 
-	def seriesToDownload = ['Touch']
-//	def seriesToDownload = ['The Big Bang Theory','Touch','Dexter','The Vampire Diaries','Once Upon a Time']
+//	def seriesToDownload = ['Touch']
+	def seriesToDownload = ['The Big Bang Theory','Touch','Dexter','The Vampire Diaries','Once Upon a Time']
 	def baseURL = 'http://www.tv-links.eu'
+	def keepProcessing = Boolean.TRUE
+	def DOWNLOAD_LINKS_LIMIT = 5
 	def name() {'TvLinks'}
 	def url()  {'http://www.tv-links.eu/tv-shows/all_links'}
-	def keepProcessing = Boolean.TRUE
 
 	@Override
 	def parse() {
@@ -26,7 +26,7 @@ class TvLinks extends Site {
 					def year = it.SPAN[1].text()
 					def link = "${baseURL}${it.@href}"
 					def released = StringUtils.isBlank(year) ? null : new SimpleDateFormat('yyyy').parse(year)
-					println "Serie: ${name} (${year}) at ${link}"
+					log.info "   Serie: ${name} (${year}) at ${link}"
 					processSerie(link, provider, name, released)
 				}
 			}
@@ -41,7 +41,7 @@ class TvLinks extends Site {
 			html(url).'**'.find{ it.@class == 'z_title cfix brd_l_dot'}.'*'.findAll{it.name().equalsIgnoreCase('div') || it.name().equalsIgnoreCase('ul')}.each { 
 				if (it.name().equalsIgnoreCase('div') && StringUtils.startsWith(it.@id.text(), 'dv_snr')) {
 					def seasonNumber = Integer.parseInt(it.@id.text().replaceAll("[\\D]", ""))
-					println "< Season ${seasonNumber} >"
+					log.info "     < Season ${seasonNumber} >"
 					season = Season.findByNumberAndSerie(seasonNumber,serie) ?: new Season(number:seasonNumber,serie:serie,episode:[]).save(failOnError:true)
 					serie.seasons << season
 				} else if(it.name().equalsIgnoreCase('ul') && it.@class=='table_shows') {
@@ -52,7 +52,7 @@ class TvLinks extends Site {
 						def airDate = ep.SPAN[2].text()
 						def releasedDate
 						try {releasedDate = new SimpleDateFormat('M/d/yyyy').parse(airDate)} catch (ParseException) {}
-						println "  [Episode ${number}: ${episodeName} (${airDate})]"
+						log.info "    [Episode ${number}: ${episodeName} (${airDate})]"
 						processEpisode(season, episodeURL, number, episodeName, releasedDate)
 					} 
 				}
@@ -64,13 +64,15 @@ class TvLinks extends Site {
 	private void processEpisode(Season season, String url, Integer number, String name, Date released) {
 		if (keepProcessing) {
 			def episode = Episode.findByNumberAndNameAndSeason(number,name,season) ?: new Episode(number:number,name:name,season:season,released:released,downloadInfo:[]).save(failOnError:true)
-			println "Processing episode ${number} at ${url} ..."
-			html(url).'**'.findAll{ it.@onclick =~ '.*frameLink.*'}.each{
-				Pattern pattern = Pattern.compile("'(.*?)'");
-				Matcher matcher = pattern.matcher(it.@onclick.text());
-				if (matcher.find()) {
-					def gateway = "${baseURL}/gateway.php?data=${matcher.group(1)}"
-					processDownloadInfo(season,episode,gateway)
+			if (episode.downloadInfo.size() <= DOWNLOAD_LINKS_LIMIT) {
+				log.info "      Processing episode ${number} at ${url} ..."
+				html(url).'**'.findAll{ it.@onclick =~ '.*frameLink.*'}.each{
+					Pattern pattern = Pattern.compile("'(.*?)'");
+					Matcher matcher = pattern.matcher(it.@onclick.text());
+					if (matcher.find()) {
+						def gateway = "${baseURL}/gateway.php?data=${matcher.group(1)}"
+						processDownloadInfo(season,episode,gateway)
+					}
 				}
 			}
 		}
@@ -79,9 +81,9 @@ class TvLinks extends Site {
 	private void processDownloadInfo(Season season, Episode episode, String gateway) {
 		if (keepProcessing) {
 			if (!DownloadInfo.findByGateway(gateway)) {
-				String location = new HTTP().getHeaderField(gateway, 'Location')
+				String location = http.getHeaderField(gateway, 'Location')
 				if (location) {
-					println "--> Inner link: ${gateway} resolved as ${location} ..."
+					log.info "      --> Inner link: ${gateway} resolved as ${location} ..."
 					def downloadInfo = new DownloadInfo(gateway:gateway,downloadLink:location,episode:episode).save(failOnError:true)
 					episode.downloadInfo << 
 					season.episodes << episode.save(failOnError:true)
@@ -89,10 +91,13 @@ class TvLinks extends Site {
 					def serie = season.serie
 					def seasonStr = season.number as String
 					def line = "${timestamp}\t${serie.provider.name}\t${serie.name}\tSeason ${seasonStr}\t${episode.name}\t${location}".concat(System.getProperty('line.separator'))
-					new File(grailsApplication.config.downloadInfo.statistics.file.path).append(line)
+					String filePath = grailsApplication.config.downloadInfo.statistics.file.path
+					new File(filePath).append(line)
 				} else {
-					println "--> Inner link: ${gateway} throws null location header ..."
+					log.info "      --> Inner link: ${gateway} throws null location header ..."
 					keepProcessing = Boolean.FALSE
+					log.info "      -----> GETTING NEW IDENTITY"
+					http.getNewIdentity()
 				}
 			}
 		}
--- src/java/org/series/crawler/HTTP.java
+++ src/java/org/series/crawler/HTTP.java
@@ -1,17 +1,25 @@
 package org.series.crawler;
 
-import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+import java.net.URLStreamHandler;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.silvertunnel.netlib.adapter.url.NetlibURLStreamHandlerFactory;
+import org.silvertunnel.netlib.api.NetFactory;
+import org.silvertunnel.netlib.api.NetLayer;
+import org.silvertunnel.netlib.api.NetLayerIDs;
+import org.silvertunnel.netlib.layer.logger.LoggingNetLayer;
+import org.silvertunnel.netlib.layer.tcpip.TcpipNetLayer;
+import org.silvertunnel.netlib.layer.tls.TLSNetLayer;
+import org.silvertunnel.netlib.layer.tor.TorNetLayer;
+import org.silvertunnel.netlib.util.TempfileStringStorage;
 
 /**
  * Get a file from the Internet.
@@ -23,7 +31,68 @@ public class HTTP {
 	 * The URL of the file we want to fetch.
 	 */
 	private URL url;
-	private static String MESSAGE = "    ====== Getting information of external website =====";
+	private URLConnection urlConnection;
+	private URLStreamHandler handler;
+	protected static NetLayer loggingTcpipNetLayer;
+	protected static NetLayer loggingTlsNetLayer;
+	protected static TorNetLayer torNetLayer;
+	protected static final Log log = LogFactory.getLog(HTTP.class);
+	private static String MESSAGE = "    ====== Getting information of external website: ${URL} =====";
+
+	public HTTP() {
+		// this.handler = getNewIdentity();
+	}
+
+	synchronized public URLStreamHandler getNewIdentity() throws IOException {
+		// do it only once
+		// if (loggingTcpipNetLayer == null) {
+		// create TCP/IP layer
+		NetLayer tcpipNetLayer = new TcpipNetLayer();
+		loggingTcpipNetLayer = new LoggingNetLayer(tcpipNetLayer, "upper tcpip  ");
+		NetFactory.getInstance().registerNetLayer(NetLayerIDs.TCPIP, loggingTcpipNetLayer);
+
+		// create TLS/SSL over TCP/IP layer
+		TLSNetLayer tlsNetLayer = new TLSNetLayer(loggingTcpipNetLayer);
+		loggingTlsNetLayer = new LoggingNetLayer(tlsNetLayer, "upper tls/ssl");
+		NetFactory.getInstance().registerNetLayer(NetLayerIDs.TLS_OVER_TCPIP, loggingTlsNetLayer);
+
+		// create TCP/IP layer for directory access (use different layer here to get different logging output)
+		NetLayer tcpipDirNetLayer = new TcpipNetLayer();
+		NetLayer loggingTcpipDirNetLayer = new LoggingNetLayer(tcpipDirNetLayer, "upper tcpip tor-dir  ");
+
+		// create Tor over TLS/SSL over TCP/IP layer
+		torNetLayer = new TorNetLayer(loggingTlsNetLayer, /* loggingT */tcpipDirNetLayer,
+				TempfileStringStorage.getInstance());
+		NetFactory.getInstance().registerNetLayer(NetLayerIDs.TOR_OVER_TLS_OVER_TCPIP, torNetLayer);
+		torNetLayer.waitUntilReady();
+		// }
+
+		// refresh net layer registration
+		NetFactory.getInstance().registerNetLayer(NetLayerIDs.TCPIP, loggingTcpipNetLayer);
+		NetFactory.getInstance().registerNetLayer(NetLayerIDs.TLS_OVER_TCPIP, loggingTlsNetLayer);
+		NetFactory.getInstance().registerNetLayer(NetLayerIDs.TOR_OVER_TLS_OVER_TCPIP, torNetLayer);
+
+		// // classic: TcpipNetLayer with NetLayerIDs.TCPIP (--> HTTP over plain TCP/IP)
+		// // anonymous: TorNetLayer with NetLayerIDs.TOR (--> HTTP over TCP/IP over Tor network)
+		// // NetLayer lowerNetLayer = NetFactory.getInstance().getNetLayerById(NetLayerIDs.TCPIP);
+		// NetLayer torNetLayer = NetFactory.getInstance().getNetLayerById(NetLayerIDs.TOR);
+		// torNetLayer.clear();
+		//
+		// // wait until TOR is ready (optional) - this is only relevant for anonymous communication:
+		// torNetLayer.waitUntilReady();
+
+		// prepare URL handling on top of the lowerNetLayer
+		NetlibURLStreamHandlerFactory factory = new NetlibURLStreamHandlerFactory(false);
+		// the following method could be called multiple times
+		// to change layer used by the factory over the time:
+		factory.setNetLayerForHttpHttpsFtp(torNetLayer);
+
+		// create the suitable URL object
+		log.debug("**********************");
+		log.warn("* NEW IDENTITY READY *");
+		log.debug("**********************");
+		return factory.createURLStreamHandler("http");
+	}
 
 	/**
 	 * Get the content of the given URL as a string.
@@ -33,22 +102,33 @@ public class HTTP {
 	 * @return The content of the URL as a string.
 	 */
 	public String getURL(String url) {
-		System.out.println(MESSAGE);
-		this.url = this.parseURLString(url);
-		URLConnection urlc = this.getHttpURLConnection(this.url);
-		return getContent(urlc);
+		return getContent(getURLConnection(url));
+	}
+
+	public String getHeaderField(String url, String field) {
+		return getURLConnection(url).getHeaderField(field);
+	}
+
+	protected URLConnection getURLConnection(String urlStr) {
+		log.debug(getDebugMessage(urlStr));
+		 return this.getTorHttpURLConnection(urlStr);
+//		return this.getHttpURLConnection(urlStr);
+	}
+
+	protected String getDebugMessage(String urlStr) {
+		return MESSAGE.replace("${URL}", urlStr);
 	}
 
 	/**
 	 * Gets the content of the given URL as an array of strings.
 	 * 
-	 * @param address
+	 * @param urlStr
 	 *            The URL of the file we want to read.
 	 * @return An array of strings containing the lines of the given file (URL).
 	 */
-	public String[] getURLasLines(String address) {
-		System.out.println(MESSAGE);
-		String all = this.getURL(address);
+	public String[] getURLasLines(String urlStr) {
+		log.debug(getDebugMessage(urlStr));
+		String all = this.getURL(urlStr);
 		all = all.replaceAll("\r", "\n");
 		return all.split("\n");
 	}
@@ -56,14 +136,14 @@ public class HTTP {
 	/**
 	 * It can check if a given URL is alive or not.
 	 * 
-	 * @param address
+	 * @param urlStr
 	 *            URL address that we want to check if it's available or not.
 	 * @return True, if it's available. False, otherwise.
 	 */
-	public boolean checkHttpUrl(String address) {
+	public boolean checkHttpUrl(String urlStr) {
 		try {
-			System.out.println(MESSAGE);
-			URL url = parseURLString(address);
+			log.debug(getDebugMessage(urlStr));
+			this.url = parseURLString(urlStr);
 			HttpURLConnection huc = (HttpURLConnection) url.openConnection();
 			int response = huc.getResponseCode();
 			if (response == 200)
@@ -75,16 +155,8 @@ public class HTTP {
 		return false;
 	}
 
-	public String getHeaderField(String address, String field) {
-		System.out.println(MESSAGE);
-		URL url = parseURLString(address);
-		URLConnection urlc = this.getHttpURLConnection(url);
-		 return urlc.getHeaderField("Location");
-	}
-
 	/*
-	 * =======================================================================
-	 * and here come the private functions
+	 * ======================================================================= and here come the private functions
 	 * =======================================================================
 	 */
 
@@ -116,8 +188,7 @@ public class HTTP {
 				return new String(s);
 			}
 		} catch (Exception e) {
-			System.err
-					.println("Error: I/O error while trying to read the network file!");
+			System.err.println("Error: I/O error while trying to read the network file!");
 			System.exit(-1);
 		}
 		return null; // it cannot arrive here
@@ -128,8 +199,7 @@ public class HTTP {
 	 * 
 	 * @param address
 	 *            The URL we want to check.
-	 * @return The URL itself if it is correct. If it's not correct, the program
-	 *         terminates.
+	 * @return The URL itself if it is correct. If it's not correct, the program terminates.
 	 */
 	private URL parseURLString(String address) {
 		URL url = null;
@@ -137,31 +207,65 @@ public class HTTP {
 		try {
 			url = new URL(address);
 		} catch (MalformedURLException e) {
-			System.err.println("Error: the URL \"" + address
-					+ "\" is malformed!");
+			System.err.println("Error: the URL \"" + address + "\" is malformed!");
 			System.exit(-1);
 		}
 		return url;
 	}
 
-	@SuppressWarnings("static-access")
-	private URLConnection getHttpURLConnection(URL url) {
-		HttpURLConnection myUC = null;
+	private URLConnection getTorHttpURLConnection(String urlStr) {
+		HttpURLConnection urlConnection = null;
 		try {
-			myUC = (HttpURLConnection) url.openConnection();
-			myUC.setInstanceFollowRedirects(false);
-			myUC.setFollowRedirects(false);
-			myUC.setRequestProperty(
-					"User-agent", // change
-					"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"); // change
-			myUC.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-			myUC.setRequestProperty("Accept-Charset", "Accept-Charset: ISO-8859-1,UTF-8;q=0.7,*;q=0.7");
-			myUC.connect();
+			URL context = null;
+			URL url = new URL(context, urlStr, this.handler);
+			this.url = url;
+
+			// /////////////////////////////////////////////
+			// the rest of this method is as for every java.net.URL object,
+			// read JDK docs to find out alternative ways:
+			// /////////////////////////////////////////////
+
+			// send request without POSTing data
+			urlConnection = HttpURLConnection.class.cast(url.openConnection());
+			HttpURLConnection.setFollowRedirects(false);
+			urlConnection.setInstanceFollowRedirects(false);
+			urlConnection
+					.setRequestProperty("User-agent",
+							"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11");
+			urlConnection.setDoInput(true);
+			urlConnection.setDoOutput(false);
+			urlConnection.connect();
+
+			// // receive and print the response
+			// BufferedReader response = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+			// String line;
+			// while ((line = response.readLine()) != null) {
+			// System.out.println(line);
+			// }
+			// response.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		this.urlConnection = urlConnection;
+		return this.urlConnection;
+	}
+
+	private URLConnection getHttpURLConnection(String urlStr) {
+		HttpURLConnection urlConnection = null;
+		try {
+			urlConnection = HttpURLConnection.class.cast(parseURLString(urlStr).openConnection());
+			urlConnection.setInstanceFollowRedirects(false);
+			HttpURLConnection.setFollowRedirects(false);
+			urlConnection
+					.setRequestProperty("User-agent",
+							"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11");
+			urlConnection.connect();
 		} catch (Exception e) {
-			System.err
-					.println("Error: network error while trying to open network connection!");
+			System.err.println("Error: network error while trying to open network connection!");
 			System.exit(-1);
 		}
-		return myUC;
+		return urlConnection;
 	}
 }
