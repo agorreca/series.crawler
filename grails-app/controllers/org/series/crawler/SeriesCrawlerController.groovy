1d2fd4eed75488bd3f0066fefaddd4a3d49386bd
Nicolas Agorreca <agorreca@gmail.com>
2012-11-18 18:24:13 -0200

    Adding some links to downloadInfoStats, supressing show.gsp and adding some logic (non finished) for show action

--- grails-app/controllers/org/series/crawler/SeriesCrawlerController.groovy
+++ grails-app/controllers/org/series/crawler/SeriesCrawlerController.groovy
@@ -1,5 +1,7 @@
 package org.series.crawler
 
+import grails.converters.JSON
+
 import org.series.crawler.site.Cucirca
 import org.series.crawler.site.TvLinks
 
@@ -10,13 +12,39 @@ class SeriesCrawlerController {
 
 	def show() {
 //		[providers:[Provider.findByName(tvLinks.name())]]
-		[providers:Provider.all]
+//		[providers:Provider.all]
+		def model = [:]
+		def episodeModel = [:]
+		def seasonModel = [:]
+		def serieModel = [:]
+//		def results = DownloadInfo.withCriteria {
+//			isNotNull('anotherDomainClass')
+//		}
+//		DownloadInfo.where {
+//			downloadLink != null
+//		}.list().each { downloadInfo ->
+//			episodeModel.put('number', downloadInfo.episode.number)
+//			episodeModel.put('name', downloadInfo.episode.name)
+//			episodeModel.put('released', downloadInfo.episode.released)
+//			episodeModel.put('')
+//		}
+
+		def episodeList = []
+		DownloadInfo.where { downloadLink != null }.list().each { it ->
+		  episodeModel = [name:it.episode.name,links:it.downloadLink]
+		  episodeList << episodeModel
+		}
+
+		def list1 = episodeList.groupBy {it.name}
+		def list2 = episodeList.groupBy {it.name}.collect { it.value.collectEntries { it } }
+		render list2 as JSON
+
 	}
 
 	def fetch() {
 		def sites = [tvLinks,cucirca]
 //		sites = [cucirca]
-//		sites = [tvLinks]
+		sites = [tvLinks]
 		CrawlerUtils.crawlSites(sites)
 		render 'LISTO'
 //		redirect(action: "show")
@@ -30,13 +58,7 @@ class SeriesCrawlerController {
 				it.save(failOnError:true)
 				count++
 			}
-//			count += DownloadInfo.executeUpdate("update DownloadInfo set downloadLink = (:link) where downloadLink like (:info)%", [link:null,info:info])
-//			DownloadInfo.where { downloadLink ==~ /^${info}/ }.deleteAll()
-//			count += DownloadInfo.createCriteria().list {
-//				like('downloadLink', "${info}%")
-//			}*.delete()
 		}
-//		redirect(action: "show")
 		render "${count} registers deleted"
 	}
 }
